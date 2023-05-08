ea8be0d2294bd3d5c8843bb6387f78621d647780
Rafael Braga <faelbragaf@gmail.com>
2023-05-08 20:34:10 -0300

    Começando a implementar Tabuleiro e Peça

--- src/tabuleiro/Posição.java
+++ src/tabuleiro/Posição.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tabuleiro;
 
-/**
- *
- * @author faelb
- */
 public class Posição {
+    
+    private Integer linha;
+    private Integer coluna;
+
+    public Posição() {
+    }
+
+    public Posição(Integer linha, Integer coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public Integer getLinha() {
+        return linha;
+    }
+
+    public void setLinha(Integer linha) {
+        this.linha = linha;
+    }
+
+    public Integer getColuna() {
+        return coluna;
+    }
+
+    public void setColuna(Integer coluna) {
+        this.coluna = coluna;
+    }
+
+    @Override
+    public String toString() {
+        return linha + ", " + coluna;
+    }
+    
+    
     
 }
